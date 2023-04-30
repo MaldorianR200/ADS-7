bb51a67ccbd836ef8621b1c3ab885b2a23500c87
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-04-30 09:46:55 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,51 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
+#include <iostream>
+#include <ostream>
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
+    SYM* next;
 };
+template <class T, int size>
+class TPQueue {
+ private:
+    SYM* head;
+    SYM* tail;
+
+ public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue() {
+        while (head)
+            pop();
+    }
+    void push(SYM a) {
+        SYM* temp = new SYM{ a.ch, a.prior, nullptr };
+        if (!head) {
+            head = temp;
+            tail = temp;
+        } else if (a.prior > head->prior) {
+            temp->next = head;
+            head = temp;
+        } else {
+            SYM* current = head;
+            while (current->next && a.prior <= current->next->prior)
+                current = current->next;
+            temp->next = current->next;
+            current->next = temp;
+        }
+    }
+    SYM pop() {
+        SYM* temp = head;
+        head = head->next;
+        return *temp;
+    }
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+};
+
+
 
 #endif  // INCLUDE_TPQUEUE_H_
